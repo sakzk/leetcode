cef3bde3c62874337dd16194decce618d688b683
sakzk <s1911490@klis.tsukuba.ac.jp>
2024-05-16 07:11:35 +0900

    Create note.py

---
+++ 33_search_in_rotated_sorted_array/note.py
@@ -0,0 +1,13 @@
+"""
+問題
+https://github.com/Exzrgs/LeetCode/pull/11#discussion_r1601304759
+
+関連問題
+https://github.com/hayashi-ay/leetcode/pull/45
+https://github.com/colorbox/leetcode/pull/10
+
+二分探索関連
+https://discord.com/channels/1084280443945353267/1235971495696662578/1237425063242891315
+インデックス関連：
+https://discord.com/channels/1084280443945353267/1235971495696662578/1236993286212943933
+"""
